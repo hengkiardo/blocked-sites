f02514d058c2852708e1f1b3b3614de02b9e3c8c
Hengkiardo <hengki.sihombing@gmail.com>
2014-05-29 23:16:29 +0700

    move parseURL function as module

---
+++ parseURL.js
@@ -0,0 +1,38 @@
+module.exports = function parseURL(url) {
+    parsed_url = {}
+
+    if ( url == null || url.length == 0 )
+        return parsed_url;
+
+    protocol_i = url.indexOf('://');
+    parsed_url.protocol = url.substr(0,protocol_i);
+
+    remaining_url = url.substr(protocol_i + 3, url.length);
+    domain_i = remaining_url.indexOf('/');
+    domain_i = domain_i == -1 ? remaining_url.length - 1 : domain_i;
+    parsed_url.domain = remaining_url.substr(0, domain_i);
+    parsed_url.path = domain_i == -1 || domain_i + 1 == remaining_url.length ? null : remaining_url.substr(domain_i + 1, remaining_url.length);
+
+    domain_parts = parsed_url.domain.split('.');
+    switch ( domain_parts.length ){
+        case 2:
+          parsed_url.subdomain = null;
+          parsed_url.host = domain_parts[0];
+          parsed_url.tld = domain_parts[1];
+          break;
+        case 3:
+          parsed_url.subdomain = domain_parts[0];
+          parsed_url.host = domain_parts[1];
+          parsed_url.tld = domain_parts[2];
+          break;
+        case 4:
+          parsed_url.subdomain = domain_parts[0];
+          parsed_url.host = domain_parts[1];
+          parsed_url.tld = domain_parts[2] + '.' + domain_parts[3];
+          break;
+    }
+
+    parsed_url.parent_domain = parsed_url.host + '.' + parsed_url.tld;
+
+    return parsed_url;
+}
